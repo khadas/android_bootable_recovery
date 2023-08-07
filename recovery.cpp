e3ef1196eabdc4b9a01376045f6267560ef3df9f
chaoqun.huang <chaoqun.huang@rock-chips.com>
2023-08-07 02:22:31 +0000

    pcba_core: merge from Android13
    Signed-off-by: chaoqun.huang <chaoqun.huang@rock-chips.com>
    Change-Id: I305d568f04da5bdfe3b90f1d4f6014469d67141d

--- recovery.cpp
+++ recovery.cpp
@@ -64,7 +64,7 @@
 #include "rkutility/rktools.h"
 #include "rkutility/sdboot.h"
 #include "rkupdate/Upgrade.h"
-//#include "pcba_core/rkfactory_test.h"
+#include "pcba_core/rkfactory_test.h"
 
 static constexpr const char* COMMAND_FILE = "/cache/recovery/command";
 static constexpr const char* LAST_KMSG_FILE = "/cache/recovery/last_kmsg";
@@ -1074,7 +1074,7 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   int retry_count = 0;
   bool security_update = false;
   std::string locale;
-//  RKFactory rkfactory;
+  RKFactory rkfactory;
   char *rkloader_update_package = nullptr;
 
   auto args_to_parse = StringVectorToNullTerminatedArray(args);
@@ -1337,7 +1337,7 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
       WipeFrp();
     }
   }else if (factory_mode != nullptr){
-    //status = (InstallResult)rkfactory.StartFactorytest(device);
+    status = (InstallResult)rkfactory.StartFactorytest(device);
     printf("do_factory_mode status=%d factory_mode=%s \n", status, factory_mode);
     exit_from_factory = 1;
   }else if (should_prompt_and_wipe_data) {
